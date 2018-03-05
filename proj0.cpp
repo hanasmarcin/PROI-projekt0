58311ee6c51ba745476d32708364a9570f682093
mahanas <36889345+mahanas@users.noreply.github.com>
2018-03-05 20:01:01 +0100

    Create proj0.cpp

---
+++ proj0.cpp
@@ -0,0 +1,21 @@
+/*Marcin Hanas
+ *Projekt 0
+ */
+
+#include <iostream>
+#include <string.h>
+#include "naglowek.h"
+
+using namespace std;
+
+
+void Napis::wyswietl()
+{
+    cout << tekst << endl;
+}
+
+int main()
+{
+    helloworld.wyswietl();
+    return 0;
+}
