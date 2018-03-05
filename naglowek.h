9f667eadb79da385196941bd4c251b874a1e1467
mahanas <36889345+mahanas@users.noreply.github.com>
2018-03-05 20:14:27 +0100

    Create naglowek.h

---
+++ naglowek.h
@@ -0,0 +1,15 @@
+#ifndef NAGLOWEK_H_INCLUDED
+#define NAGLOWEK_H_INCLUDED
+
+class Napis
+{
+private:
+    char tekst[12]="Hello world";
+public:
+    void wyswietl();
+};
+
+Napis helloworld;
+
+
+#endif // NAGLOWEK_H_INCLUDED
